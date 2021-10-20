f051ac3cb2bceafe31f5786cef26fcf2037520f7
kbzaso <alejandro.sapa@gmail.com>
2021-10-20 13:11:32 -0400

    cors

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+var express = require('express');
+var cors = require('cors');
+var app = express();
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+app.use(cors());
